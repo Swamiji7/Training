1ba883558e64043929779624bca8aee9521a9980
vshadmin <vshadmin@Infva06522>
2019-06-08 16:23:25 -0700

    jdbc select

--- hello/src/com/lit/training/day7/jdbc/Appmain.java
+++ hello/src/com/lit/training/day7/jdbc/Appmain.java
@@ -1,14 +1,25 @@
 package com.lit.training.day7.jdbc;
 
+import java.util.List;
+
 public class Appmain {
 
 	public static void main(String[] args) {
 		ProductDao dao = new ProductDao();
-		Product p = new Product();
-		p.setId(2);
+		/*Product p = new Product();
+		/*p.setId(2);
 		p.setName("Nokia 6000");
 		p.setPrice(10000);
 		p.setQuantity(99);
 		dao.add(p);
+		*/
+		
+		List<Product> products=dao.fetchAll();
+		for(Product p : products) {
+		System.out.println("ID : "+p.getId());
+		System.out.println("Name : "+p.getName());
+		System.out.println("Price : "+p.getPrice());
+		System.out.println("Quantity : "+p.getQuantity());
+		}
 	}
 }
--- hello/src/com/lit/training/day7/jdbc/ProductDao.java
+++ hello/src/com/lit/training/day7/jdbc/ProductDao.java
@@ -3,7 +3,10 @@ package com.lit.training.day7.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 //Dao-Data Access Object
 /*
@@ -21,6 +24,7 @@ public class ProductDao {
 	public void add(Product product) {
 		Connection conn = null; // helps the connection between the app
 		PreparedStatement stmt = null; // helps us to execute SQL Statements
+		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
@@ -43,5 +47,40 @@ public class ProductDao {
 			} catch (Exception e) {
 			}
 		}
+	}
+
+	public List<Product> fetchAll() {
+		Connection conn = null; // helps the connection between the app
+		PreparedStatement stmt = null; // helps us to execute SQL Statements
+		ResultSet rs = null; // helps to retrieve the result of the sql query
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
+
+			String sql = "select * from TBL_PRODUCT where quantity >= ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, 10);
+			rs = stmt.executeQuery();
+			List<Product> products = new ArrayList<Product>();
+			while(rs.next()) { // for each row
+				Product p = new Product();
+				p.setId(rs.getInt(1));
+				p.setName(rs.getString(2));
+				p.setPrice(rs.getDouble(3));
+				p.setQuantity(rs.getInt(4));
+				products.add(p);
+			}
+			return products;
+		} catch (ClassNotFoundException e) {
+			System.out.println("Jdbc driver not found..");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return null;
 	}
 }
