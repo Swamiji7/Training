11a534b4fc796eb999d6d3884beb8d708a0278b9
vshadmin <vshadmin@Infva06522>
2019-06-08 19:29:57 -0700

    Mini Proj

--- hello/src/com/miniproject/CollegeDao.java
+++ hello/src/com/miniproject/CollegeDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+ 
 public class CollegeDao {
 	
 	public void add(College coll) {
